b8c5cdf9ee43b5b8e01934bb8a098c0fcc81bd02
volume02 <borsukboris@gmail.com>
2023-10-31 16:02:35 +0700

    animated enemy

--- Assets/Scripts/MoveToPlayer.cs
+++ Assets/Scripts/MoveToPlayer.cs
@@ -8,8 +8,11 @@ public class MoveToPlayer : MonoBehaviour
     public Transform rightBound;
 
     public GameObject player;
+    public GameObject enemyMesh;
+    Animator animator;
 
     Vector3 startPosition;
+    bool isMoving = false;
 
     float speed = 5;
     // Start is called before the first frame update
@@ -17,22 +20,40 @@ public class MoveToPlayer : MonoBehaviour
     {
         startPosition = transform.position;
         player = GameObject.FindWithTag("Player");
+        animator = enemyMesh.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 direction = Vector3.zero;
+        isMoving = Mathf.Abs((startPosition - transform.position).x) > 0.1;
         var playerPos = player.transform.position;
+
         if (playerPos.x > leftBound.position.x && playerPos.x < rightBound.position.x)
         {
-            var direction = (playerPos - transform.position).normalized;
-
-            transform.Translate(new Vector3(direction.x , 0, 0) * speed * Time.deltaTime);
-        } else
+            direction = (playerPos - transform.position).normalized;
+        }
+        else if(isMoving)
         {
-            var direction =  (startPosition - transform.position).normalized;
-            transform.Translate(new Vector3(direction.x, 0, 0) * speed * Time.deltaTime);
+            direction = (startPosition - transform.position).normalized;
         }
 
+        transform.Translate(new Vector3(direction.x, 0, 0) * speed * Time.deltaTime, Space.World);
+        animator.SetBool("isMoving", isMoving);
+        TurnEnemy(direction);
+
+    }
+
+    void TurnEnemy(Vector3 direction)
+    {
+        if (direction != Vector3.right)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
     }
 }
